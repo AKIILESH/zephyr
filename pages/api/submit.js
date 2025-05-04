2cb4b4b12ec8029f87392377cf7138787d6171d8
AKILESH <155362761+AKIILESH@users.noreply.github.com>
2025-05-04 21:45:35 +0530

    Update submit.js

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   if (req.method === 'POST') {
-    const { name, email, message } = req.body;
-    console.log({ name, email, message }); // or save to database
+    const { name, pn,email, message } = req.body;
+    console.log({ name,pn, email, message }); // or save to database
     res.status(200).json({ success: true });
   } else {
     res.status(405).send('Method Not Allowed');
